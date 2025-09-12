7172edfa4e875858eef7cf5f3bda57b2b00d7fcf
kPherox <kphrx@kpherox.dev>
2025-09-13 02:45:25 +0900

    add `--use-harmony` option

--- Source/ModTemplate.cs
+++ Source/ModTemplate.cs
@@ -1,5 +1,8 @@
 ﻿using RimWorld;
 using Verse;
+#if (UseHarmony)
+using HarmonyLib;
+#endif
 
 namespace ModTemplate
 {
@@ -8,6 +11,10 @@ namespace ModTemplate
     {
         static ModTemplate()
         {
+#if (UseHarmony)
+            var harmony = new Harmony("AuthorName.ModTemplate");
+            harmony.PatchAll();
+#endif
             Log.Message("ModTemplate loaded successfully!");
         }
     }
