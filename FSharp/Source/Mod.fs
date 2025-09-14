d67767ecc6d9e7a05fadbe7992a70d8f5b17ea8f
kPherox <kphrx@kpherox.dev>
2025-09-15 01:17:59 +0900

    support F# modding

---
+++ FSharp/Source/Mod.fs
@@ -0,0 +1,16 @@
+﻿namespace ModTemplate
+
+open RimWorld;
+open Verse;
+#if (useHarmony)
+open HarmonyLib;
+#endif
+
+[<StaticConstructorOnStartup>]
+type StartupConstructor() =
+  static do
+#if (useHarmony)
+    let harmony = Harmony("AuthorName.ModTemplate")
+    do harmony.PatchAll()
+#endif
+    Log.Message("ModTemplate loaded successfully!")
